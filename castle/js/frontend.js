dd4f25ef91252e7e6cafab95bb35dc7e8e8d5ba9
johibkhan2 <johib.khan@gmail.com>
2017-07-15 09:28:15 +0530

    vuejssample project

---
+++ castle/js/frontend.js
@@ -0,0 +1,152 @@
+var app = new Vue({
+  el: '#property-gallery',
+  data: {
+    properties: [
+      {
+        "name": "Park Avenue Apartment",
+        "address": "45 Regent Street, London, UK",
+        "featured": false,
+        "latest": true,
+        "sale": true,
+        "rent": false,
+        "price": "$8,600 Per Month",
+        "amenities": {
+          "space": "4000 sq ft",
+          "rooms": "2 Office Rooms",
+          "bathroom": "1 Bathroom",
+          "lounge": "TV Lounge",
+          "garage": "1 Garage",
+          "added": "5 Days Ago"
+        }
+      },{
+        "name": "Park Avenue Apartment",
+        "address": "45 Regent Street, London, UK",
+        "featured": true,
+        "latest": true,
+        "sale": false,
+        "rent": true,
+        "price": "$8,600 Per Month",
+        "amenities": {
+          "space": "4000 sq ft",
+          "rooms": "2 Office Rooms",
+          "bathroom": "1 Bathroom",
+          "lounge": "TV Lounge",
+          "garage": "1 Garage",
+          "added": "5 Days Ago"
+        }
+      },{
+        "name": "Park Avenue Apartment",
+        "address": "45 Regent Street, London, UK",
+        "featured": true,
+        "latest": true,
+        "sale": true,
+        "rent": false,
+        "price": "$8,600 Per Month",
+        "amenities": {
+          "space": "4000 sq ft",
+          "rooms": "2 Office Rooms",
+          "bathroom": "1 Bathroom",
+          "lounge": "TV Lounge",
+          "garage": "1 Garage",
+          "added": "5 Days Ago"
+        }
+      },{
+        "name": "Park Avenue Apartment",
+        "address": "45 Regent Street, London, UK",
+        "featured": true,
+        "latest": true,
+        "sale": false,
+        "rent": true,
+        "price": "$8,600 Per Month",
+        "amenities": {
+          "space": "4000 sq ft",
+          "rooms": "2 Office Rooms",
+          "bathroom": "1 Bathroom",
+          "lounge": "TV Lounge",
+          "garage": "1 Garage",
+          "added": "5 Days Ago"
+        }
+      },{
+        "name": "Park Avenue Apartment",
+        "address": "45 Regent Street, London, UK",
+        "featured": false,
+        "latest": true,
+        "sale": true,
+        "rent": false,
+        "price": "$8,600 Per Month",
+        "amenities": {
+          "space": "4000 sq ft",
+          "rooms": "2 Office Rooms",
+          "bathroom": "1 Bathroom",
+          "lounge": "TV Lounge",
+          "garage": "1 Garage",
+          "added": "5 Days Ago"
+        }
+      },{
+        "name": "Park Avenue Apartment",
+        "address": "45 Regent Street, London, UK",
+        "featured": true,
+        "latest": true,
+        "sale": false,
+        "rent": true,
+        "price": "$8,600 Per Month",
+        "amenities": {
+          "space": "4000 sq ft",
+          "rooms": "2 Office Rooms",
+          "bathroom": "1 Bathroom",
+          "lounge": "TV Lounge",
+          "garage": "1 Garage",
+          "added": "5 Days Ago"
+        }
+      },{
+        "name": "Park Avenue Apartment",
+        "address": "45 Regent Street, London, UK",
+        "featured": false,
+        "latest": true,
+        "sale": true,
+        "rent": false,
+        "price": "$8,600 Per Month",
+        "amenities": {
+          "space": "4000 sq ft",
+          "rooms": "2 Office Rooms",
+          "bathroom": "1 Bathroom",
+          "lounge": "TV Lounge",
+          "garage": "1 Garage",
+          "added": "5 Days Ago"
+        }
+      },{
+        "name": "Park Avenue Apartment",
+        "address": "45 Regent Street, London, UK",
+        "featured": false,
+        "latest": true,
+        "sale": false,
+        "rent": true,
+        "price": "$8,600 Per Month",
+        "amenities": {
+          "space": "4000 sq ft",
+          "rooms": "2 Office Rooms",
+          "bathroom": "1 Bathroom",
+          "lounge": "TV Lounge",
+          "garage": "1 Garage",
+          "added": "5 Days Ago"
+        }
+      },{
+        "name": "Park Avenue Apartment",
+        "address": "45 Regent Street, London, UK",
+        "featured": false,
+        "latest": true,
+        "sale": true,
+        "rent": false,
+        "price": "$8,600 Per Month",
+        "amenities": {
+          "space": "4000 sq ft",
+          "rooms": "2 Office Rooms",
+          "bathroom": "1 Bathroom",
+          "lounge": "TV Lounge",
+          "garage": "1 Garage",
+          "added": "5 Days Ago"
+        }
+      }
+    ]
+  }
+})
